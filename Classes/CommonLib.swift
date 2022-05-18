d2cc6288d0a7167cbbe9b5fac768a9595dcde295
edo oktarifa <edooktarifa99@gmail.com>
2022-05-18 14:24:12 +0700

    remove rxcocoa

--- Classes/CommonLib.swift
+++ Classes/CommonLib.swift
@@ -7,25 +7,6 @@
 
 import Foundation
 import UIKit
-import RxCocoa
-
-extension UIView {
-    public func roundCorners(corners: UIRectCorner, radius: CGFloat) {
-        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        let mask = CAShapeLayer()
-        mask.path = path.cgPath
-        layer.mask = mask
-    }
-}
-
-extension BehaviorRelay where Element: RangeReplaceableCollection {
-
-    public func add(element: Element.Element) {
-        var array = self.value
-        array.append(element)
-        self.accept(array)
-    }
-}
 
 extension UIViewController{
     public func presentAlertWithTitle(title: String, message: String, options: String..., completion: @escaping (Int?) -> Void) {
